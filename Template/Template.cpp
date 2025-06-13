431c913a21e2d0061453e31b137b0b02846b8e0f
lunarzDev <lunarzDev@outlook.com>
2025-06-13 00:50:36 -0500

    Template

--- Template/Template.cpp
+++ Template/Template.cpp
@@ -1,19 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Aliases
 using ll = long long;
 using ld = long double;
 using ull = unsigned long long;
 
+// Constants
 constexpr ll MOD = 1e9 + 7;
 constexpr ld EPS = 1e-9;
 constexpr ll INF = 2e18;
 
+// Macros
+#define F first
+#define S second
+#define int long long
+
+// Hacks
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+template <typename T1, typename T2> // Integer key random hash
+using safe_map = unordered_map<T1, T2, custom_hash>;
+
+// Operator overloads
+template <typename T1, typename T2> // cin >> pair<T1, T2>
+istream& operator>>(istream &istream, pair<T1, T2> &p) { return (istream >> p.first >> p.second); }
+
+template <typename T> // cin >> vector<T>
+istream& operator>>(istream &istream, vector<T> &v) {
+    for(auto &it : v)
+        cin >> it;
+    return istream;
+}
+
+template<typename T1, typename T2> // cout << pair<T1, T2>
+ostream& operator<<(ostream &ostream, const pair<T1, T2> &p) { return (ostream << p.first << " " << p.second); }
+template<typename T> // cout << vector<T>
+ostream& operator<<(ostream &ostream const vector<T> &c) { for (auto &it : c) cout << it << " "; return ostream; }
+
+// Utility functions
+
+
+void solve(){
+    
+}
+
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin >> t;
-    
     while(t--){
-        
+        solve();
     }
-    
+    return 0
+} 
