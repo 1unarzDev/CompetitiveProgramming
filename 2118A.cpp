064ba76775733735efd2808032e4bedb5005849d
lunarzDev <lunarzDev@outlook.com>
2025-06-14 16:05:18 -0500

    Fix

--- 2118A.cpp
+++ 2118A.cpp
@@ -67,7 +67,15 @@ vector<bool> sieve(int n) {
 }
 
 void solve(){
-
+    int n, k;
+    cin >> n >> k;
+    for(int i = 0; i < k; i++){
+        cout << 1;
+    }        
+    for(int i = 0; i < n - k; i++){
+        cout << 0;
+    }
+    cout << endl;
 }
 
 int32_t main() {
